2a8075cfc5ad6b50ca46e9a3b42aa48ebc0f9d07
Vagan Galstian <vagamagajaga@yandex.ru>
2022-12-20 22:06:04 +0400

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Vagan Galstian on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
